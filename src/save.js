a64031acbd0a5abf7299ad40413465491da2dd2d
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-04-14 14:04:41 -0500

    feat: :sparkles: It is added that the user can save the character of his preference in the local storage
    The design of the entire website clearly needs to be improved. The page where the characters saved by users can be listed needs to be added.

---
+++ src/save.js
@@ -0,0 +1,25 @@
+document.getElementById("form__character").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const character = {
+        name: document.getElementById("name").value,
+        gender: document.getElementById("gender").value,
+        races: document.getElementById("races").value,
+        classes: document.getElementById("classes").value,
+        armor: document.getElementById("armor").value,
+        weapon: document.getElementById("weapon").value,
+        force: document.getElementById("force").value,
+        skill: document.getElementById("skill").value,
+        intelligence: document.getElementById("intelligence").value,
+    };
+    console.log(character);
+    saveCharacter(character)
+})
+function saveCharacter(character) {
+    const savedCharacters = JSON.parse(localStorage.getItem("characters")) || [];
+    savedCharacters.push(character);
+    localStorage.setItem("characters", JSON.stringify(savedCharacters));
+    console.log(savedCharacters);
+    alert("¡Character saved successfully!");
+    document.getElementById("form__character").reset();
+}
