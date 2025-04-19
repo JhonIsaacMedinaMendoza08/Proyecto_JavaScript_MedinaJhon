7f55616cd16b613ab7deff7e85c290aa7f8bcfa4
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-04-19 17:02:12 -0500

    feat: :lipstick: The design of the specific character details area is finalized.
    The responsive version needs to be added to the entire website.

--- src/details.js
+++ src/details.js
@@ -17,20 +17,37 @@ document.addEventListener("DOMContentLoaded", () => {
     contenedor.innerHTML = "<p>The requested character was not found.</p>";
     return; // Termina la ejecución
   }
+  const raceImages = {
+    "dragonborn": "assets/races/dragonborn.png",
+    "dwarf": "assets/races/dwarf.png",
+    "elf": "assets/races/elf.webp",
+    "gnome": "assets/races/gnome.webp",
+    "half-elf": "assets/races/half-elf.png",
+    "half-orc": "assets/races/orc.png",
+    "halfling": "assets/races/halfling.png",
+    "human": "assets/races/human.webp",
+    "tiefling": "assets/races/tiefling.webp",
+    "default": "assets/races/default.png"
+};
   // Si el índice es válido, obtiene el personaje correspondiente
   const character = characters[index];
   // Muestra la información detallada del personaje en el contenedor
+  const imageSrc = raceImages[character.races] || raceImages["default"];
   contenedor.innerHTML = `
     <div class="card">
+      <img src="${imageSrc}" alt="${character.races}" class="race-image" />
       <h2>${character.name}</h2>
-      <p><strong>Género:</strong> ${character.gender}</p>
-      <p><strong>Raza:</strong> ${character.races}</p>
-      <p><strong>Clase:</strong> ${character.classes}</p>
-      <p><strong>Armadura:</strong> ${character.armor}</p>
-      <p><strong>Arma:</strong> ${character.weapon}</p>
-      <p><strong>Estadísticas:</strong> Fuerza: ${character.force}, Destreza: ${character.skill}, Inteligencia: ${character.intelligence}</p>
-      <p><strong>Habilidad Especial:</strong> ${character.ability}</p>
-      <p><strong>Accesorios:</strong> ${character.accesories}</p>
+      <p><strong>Gender:</strong> ${character.gender}</p>
+      <p><strong>Races:</strong> ${character.races}</p>
+      <p><strong>Class:</strong> ${character.classes}</p>
+      <p><strong>Armor:</strong> ${character.armor}</p>
+      <p><strong>Weapon:</strong> ${character.weapon}</p>
+      <p><strong>Statistics:</strong> <br>
+      Force: ${character.force}, <br>
+      Skill: ${character.skill}, <br>
+      Intelligence: ${character.intelligence}</p>
+      <p><strong>Special Ability:</strong> ${character.ability}</p>
+      <p><strong>Accessories:</strong> ${character.accesories}</p>
     </div>
   `;
 });
